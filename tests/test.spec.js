5ffdfc595e0fa5cee91961cb9dd2047dd7392c0c
adi <adistudent45@gmail.com>
2024-08-08 22:48:00 +0300

    taet

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,11 +1,22 @@
 import request from 'supertest';
 import app from '../index.js';
 
+// שמירת אובייקט השרת
+let server;
+
+before((done) => {
+  server = app.listen(done); // הפעל את השרת לפני הבדיקות
+});
+
 describe("API routes", () => {
   it('should return pong with team number from /ping', (done) => {
-    request(app)
+    request(server) // השתמש באובייקט השרת
       .get('/ping')
       .expect(200)
-      .expect(`pong TripTips6`, done);
+      .expect('pong TripTips6', done);
   });
+});
+
+after((done) => {
+  server.close(done); // סגור את השרת לאחר הבדיקות
 });
