6137f728261dc7f29a3d1d5a90e56c3c95ba99fc
adi <adistudent45@gmail.com>
2024-08-08 23:52:09 +0300

    TEMP

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,25 +1,12 @@
 import request from 'supertest';
 import app from '../index.js';
 
-// שמירת אובייקט השרת
-let server;
-
-before((done) => {
-  server = app.listen(done); // הפעל את השרת לפני הבדיקות
-});
-
 describe("API routes", () => {
   it('should return pong with team number from /ping', (done) => {
-    request(server) // השתמש באובייקט השרת
+    request(app) // השתמש באובייקט השרת
       .get('/ping')
       .expect(200)
       .expect('pong TripTips6', done);
   });
 });
 
-after((done) => {
-  server.close(() => {
-    console.log('Server closed'); // להדפיס הודעה שהשרת נסגר
-    done(); // סיים את הבדיקה
-  });
-});
